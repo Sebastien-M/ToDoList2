1776469d4adf5403c5a6a30012719aebdadc5249
Sébastien Mandaba <seb.mandaba@gmail.com>
2017-05-11 23:43:47 +0200

    Red button changed

--- toDo2.js
+++ toDo2.js
@@ -1,24 +1,23 @@
-
 let ul = document.getElementById("ul");
 let saisi = document.getElementById("titre");
 let tableau = [];
 
-document.body.querySelector("form").addEventListener('submit', function (e) {
+document.body.querySelector("form").addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    if (saisi.value === ""){
-        document.body.querySelector("style").innerHTML = "#buttonSubmit{animation-name:red;animation-duration:1s;}#buttonSubmit:hover{animation-name:red;animation-duration:1s;}@keyframes red{from{background-color:red;}to{background-color:white;}}"
-    }
-    else{
+
+    if (saisi.value === "") {
+        document.body.querySelector("style").innerHTML = "#buttonSubmit{animation-name:red;animation-duration:1s;}#buttonSubmit:hover{background-color:red;animation-name:red;animation-duration:1s;}@keyframes red{from{background-color:red;}to{background-color:white;}}";
+    } else {
         document.body.querySelector("style").innerHTML = "";
         tableau.push(saisi.value);
         aff();
     }
-    
+
 });
+
 function aff() {
     ul.innerHTML = "";
-    
+
     for (let i = 0; i < tableau.length; i++) {
         let li = document.createElement("li");
         li.textContent = tableau[i];
@@ -26,9 +25,10 @@ function aff() {
         let buton = document.createElement("button");
         buton.textContent = "Delete";
         li.appendChild(buton);
-        buton.onclick = function () {
+        buton.onclick = function() {
             tableau.splice(i, 1);
             aff();
         };
     }
 }
+// http://stackoverflow.com/questions/20340138/remove-blue-border-from-css-custom-styled-button-in-chrome
